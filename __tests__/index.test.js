a2e0f91a581ee3bf504b335f38bd1b284d416cf8
Daniel <danielcjamesdj@gmail.com>
2022-11-15 10:48:21 +0000

    Refactor GET /api/review/:review_id to be more robust

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -75,16 +75,18 @@ describe("/api/reviews/:review_id", () => {
       .get("/api/reviews/1")
       .expect(200)
       .then((response) => {
-        expect(response.body.selectedReview).toMatchObject({
-          review_id: expect.any(Number),
-          title: expect.any(String),
-          review_body: expect.any(String),
-          designer: expect.any(String),
-          review_img_url: expect.any(String),
-          votes: expect.any(Number),
-          category: expect.any(String),
-          owner: expect.any(String),
-          created_at: expect.any(String),
+        console.log(response.body.review);
+        expect(response.body.review).toMatchObject({
+          review_id: 1,
+          title: "Agricola",
+          review_body: "Farmyard fun!",
+          designer: "Uwe Rosenberg",
+          review_img_url:
+            "https://www.golenbock.com/wp-content/uploads/2015/01/placeholder-user.png",
+          votes: 1,
+          category: "euro game",
+          owner: "mallionaire",
+          created_at: "2021-01-18T10:00:20.514Z",
         });
       });
   });
