a64e67791cf3e97e693a8d1527fa88d57f5680bb
Daniel <danielcjamesdj@gmail.com>
2022-11-15 13:42:56 +0000

    Fix GET /api/reviews query to return correct comment count

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -3,10 +3,13 @@ const db = require("../db/connection.js");
 exports.selectReviews = () => {
   return db
     .query(
-      `SELECT owner, title, review_id, category, review_img_url, created_at, votes, designer, COUNT(review_id) AS comment_count
-         FROM reviews GROUP BY owner, title, review_id, category, review_img_url, created_at, votes, designer
-         ORDER BY created_at DESC;
-         `
+      `
+      SELECT reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.review_id) AS comment_count
+      FROM reviews 
+      LEFT JOIN comments ON reviews.review_id = comments.review_id
+      GROUP BY reviews.review_id
+      ORDER BY created_at DESC;
+      `
     )
     .then((response) => {
       return response.rows;
