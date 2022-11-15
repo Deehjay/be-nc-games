d77d0795dd30de8584f5a9caeef51ba26ef75b10
Daniel <danielcjamesdj@gmail.com>
2022-11-15 11:56:48 +0000

    Remove console.log from controller

--- controllers/comments.controllers.js
+++ controllers/comments.controllers.js
@@ -2,7 +2,6 @@ const { selectCommentsByReviewId } = require("../models/comments.models");
 
 exports.getCommentsByReviewId = (req, res, next) => {
   const { review_id } = req.params;
-  console.log(req.params);
   selectCommentsByReviewId(review_id)
     .then((comments) => {
       res.status(200).send({ comments });
