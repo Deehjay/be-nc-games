be89956fc5365a82f0d07a0bda482a79a79b6a06
Daniel <danielcjamesdj@gmail.com>
2022-11-16 12:07:23 +0000

    Create test for get api users

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -353,3 +353,21 @@ describe("/api/reviews/:review_id/comments", () => {
       });
   });
 });
+
+describe("/api/users", () => {
+  test("GET - 200: Responds with an array of users", () => {
+    return request(app)
+      .get("/api/users")
+      .expect(200)
+      .then((response) => {
+        expect(response.body.users.length).toBe(4);
+        response.body.users.forEach((user) => {
+          expect(user).toMatchObject({
+            username: expect.any(String),
+            name: expect.any(String),
+            avatar_url: expect.any(String),
+          });
+        });
+      });
+  });
+});
