d1ce9b62f30b85d08ff058ca7667ef5ee1ceea16
Daniel <danielcjamesdj@gmail.com>
2022-11-16 16:42:05 +0000

    Alter model to include comment_count

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -21,8 +21,11 @@ exports.selectReviewById = (review_id) => {
   return db
     .query(
       `
-      SELECT * FROM reviews
-      WHERE review_id = $1`,
+      SELECT reviews.owner, review_body, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.review_id)::INT AS comment_count
+      FROM reviews 
+      LEFT JOIN comments ON reviews.review_id = comments.review_id
+      WHERE reviews.review_id = $1
+      GROUP BY reviews.review_id`,
       [review_id]
     )
     .then((response) => {
