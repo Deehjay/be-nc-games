931b7cfa853f625aa78423579f916643b9429176
Daniel <danielcjamesdj@gmail.com>
2022-11-17 10:51:08 +0000

    Alter model and controller for GET /api/reviews to accept queries

--- models/reviews.models.js
+++ models/reviews.models.js
@@ -1,20 +1,52 @@
 const db = require("../db/connection.js");
 const { checkReviewExists } = require("../db/seeds/utils.js");
 
-exports.selectReviews = () => {
-  return db
-    .query(
-      `
-      SELECT reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.review_id) AS comment_count
-      FROM reviews 
-      LEFT JOIN comments ON reviews.review_id = comments.review_id
-      GROUP BY reviews.review_id
-      ORDER BY created_at DESC;
-      `
-    )
-    .then((response) => {
-      return response.rows;
-    });
+exports.selectReviews = (category, sort_by = "created_at", order = "desc") => {
+  const validColumns = [
+    "review_id",
+    "title",
+    "review_body",
+    "designer",
+    "review_img_url",
+    "votes",
+    "category",
+    "owner",
+    "created_at",
+    "comment_count",
+  ];
+
+  const validCategories = ["euro game", "dexterity", "social deduction"];
+
+  let queryValues = [];
+
+  if (!validColumns.includes(sort_by)) {
+    return Promise.reject({ status: 400, msg: "Invalid sort query" });
+  }
+
+  if (!["asc", "desc"].includes(order)) {
+    return Promise.reject({ status: 400, msg: "Invalid order query" });
+  }
+
+  let queryStr = `
+    SELECT reviews.owner, title, reviews.review_id, category, review_img_url, reviews.created_at, reviews.votes, reviews.designer, COUNT(comments.review_id)::INT AS comment_count
+    FROM reviews 
+    LEFT JOIN comments ON reviews.review_id = comments.review_id
+   `;
+
+  if (category) {
+    if (!validCategories.includes(category)) {
+      return Promise.reject({ status: 400, msg: "Invalid category query" });
+    } else {
+      queryValues.push(category);
+      queryStr += ` WHERE category = $1`;
+    }
+  }
+
+  queryStr += ` GROUP BY reviews.review_id ORDER BY ${sort_by} ${order};`;
+
+  return db.query(queryStr, queryValues).then((response) => {
+    return response.rows;
+  });
 };
 
 exports.selectReviewById = (review_id) => {
